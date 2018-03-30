c2222791be155889dfbf95aa5a7114dd4b67e812
andry.baka <andry.baka@mitrais.com>
2018-03-30 13:21:23 +0700

    fix: First attempt for style

--- src/App.js
+++ src/App.js
@@ -5,14 +5,31 @@ import { Counter } from './counter'
 import { TreeView } from './treeView'
 import { About } from './about'
 
+import Grid from 'material-ui/Grid';
+import Button from 'material-ui/Button';
+
 const App = () => (
   <Router>
     <div>
-      <h1>IOOF Code Challenge</h1>
+      <Grid container spacing={24} style={{ color: '#ffffff', backgroundColor: '#2e404c' }}>
+        <Grid item xs={8}>
+          <h1>IOOF Code Challenge</h1>
+        </Grid>
+        <Grid item xs={4}>
+          <h2>Andry Baka</h2>
+        </Grid>
+      </Grid>
       <Navigation />
       <Switch>
         <Route exact path="/">
-          <p>Please read README.md</p>
+          <div>
+            <div style={ { backgroundImage: `url("/public/background.jpg")` } } >
+            <Button variant="raised" style={{ margin: 100 }}>
+              Set a default value for your counter by pressing my counter.
+              Then create a tree on the tree view tab.
+            </Button>
+            </div>
+          </div>
         </Route>
         <Route path="/counter" component={Counter} />
         <Route path="/tree-view" component={TreeView} />
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,11 +1,67 @@
 import React from 'react'
-import { NavLink } from "react-router-dom"
+import { NavLink, Link } from "react-router-dom"
 
-export default () => (
-  <ul>
-    <li><NavLink exact to='/'>Home</NavLink></li>
-    <li><NavLink to='/counter'>Counter</NavLink></li>
-    <li><NavLink to='/tree-view'>Tree View</NavLink></li>
-    <li><NavLink to='/about'>About</NavLink></li>
-  </ul>
-)
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Tabs, { Tab } from 'material-ui/Tabs';
+import Typography from 'material-ui/Typography';
+
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    marginTop: theme.spacing.unit,
+    backgroundColor: '#2e404c'
+  },
+});
+
+class SimpleTabs extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Tabs
+            style={{ color: '#ffffff', backgroundColor: '#2e404c' }} 
+            indicatorColor='#4CAF50'
+            value={value}
+            onChange={this.handleChange}
+          >
+            <Tab label="Home" component={Link} to="/counter" />
+            <Tab label="My Counter" component={Link} to="/counter" />
+            <Tab label="Tree View" component={Link} to="/tree-view" />
+            <Tab label="About" component={Link} to="/about" />
+          </Tabs>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+SimpleTabs.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleTabs);
