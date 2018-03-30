e1fbd6eab9e09e9879b9c1be8a0f30650fae948f
andry.baka <andry.baka@mitrais.com>
2018-03-30 09:45:29 +0700

    feat: Task 1 - Delayed increment and decrement

--- src/counter/actions.js
+++ src/counter/actions.js
@@ -1,10 +1,24 @@
 export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
 
-export const increment = () => ({
-  type: INCREMENT
-})
+const DELAY = 1000;
 
-export const decrement = () => ({
-  type: DECREMENT
-})
+export const increment = () => {
+  return dispatch => {
+    return setTimeout(() => {
+      dispatch({
+        type: INCREMENT
+      })
+    }, DELAY)
+  }
+}
+
+export const decrement = () => {
+  return dispatch => {
+    return setTimeout(() => {
+      dispatch({
+        type: DECREMENT
+      })
+    }, DELAY)
+  }
+}
